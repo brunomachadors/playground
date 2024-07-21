b80f2d31f70789a466490b785ebb7ac58f5bdae1
brunomachadors <brunomachadors@gmail.com>
2024-07-21 12:34:46 +0100

    Updating instructions

--- src/app/components/Instructions.tsx
+++ src/app/components/Instructions.tsx
@@ -8,8 +8,8 @@ export function InstructionsLogin() {
       </p>
       <ul className="list-disc list-inside mb-6">
         <li>
-          Quando utilizar username e senha corretos, deve retornar "usuário
-          logado".
+          Quando utilizar username e senha corretos, deve retornar{' '}
+          <strong>usuário logado</strong>.
         </li>
         <li>Login ou senha incorretos devem retornar uma mensagem de erro.</li>
         <li>Três senhas incorretas bloqueiam a conta temporariamente.</li>
